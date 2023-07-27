cd174255ef00c02685b9a227cb50c817874cd823
MertMuratBakir <125885626+MertMuratBakir@users.noreply.github.com>
2023-07-27 19:29:20 +0300

    operators

---
+++ 6_operatörler/script.js
@@ -0,0 +1,40 @@
+let sonuc;
+let a = 10, b = 20, c = 30
+
+
+// 1- Aritmetik Operatörler
+sonuc = a + b;
+sonuc = a - b;
+sonuc = a * b;
+sonuc = a / b;
+// "%" operatörü kalanı verir
+sonuc = a % b;
+
+sonuc = a++; // a + 1
+sonuc = ++a;
+sonuc = --a;
+sonuc = a++; 
+
+
+// 2- Atama Operatörler
+sonuc = a;
+sonuc += a; // sonuc = sonuc + a içeriğin üzerine değer ekler.
+sonuc -= a; // sonuc = sonuc - a
+sonuc *= a; // sonuc = sonuc * a
+sonuc /= a; // sonuc = sonuc / a
+sonuc %= a; // sonuc = sonuc % a
+
+
+
+// 3- Karşılaştırma Operatörler
+sonuc = (a == b) // a değeri b değerine eşit olup olmadığına bakılıyor.
+sonuc = (a != b) // eşit değil.
+sonuc = (3 == 4)
+sonuc = (a > b)
+sonuc = (a < b)
+sonuc = (a <= b)
+sonuc = (a >= b)
+
+// 4- Mantıksal Operatörler
+
+console.log(sonuc);
