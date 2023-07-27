e72ade9f6e0e60d5f5f4fbdcb98aa19f832aa6a6
MertMuratBakir <125885626+MertMuratBakir@users.noreply.github.com>
2023-07-27 18:34:34 +0300

    variable type

---
+++ 4_js_degisken-turleri/script.js
@@ -0,0 +1,24 @@
+var urunAdi = "iphone 13";  // string
+let urunFiyat = 15000; // number
+
+console.log(typeof urunAdi);
+console.log(typeof urunFiyat);
+
+let sayi1 = 10;
+let sayi2 = 10;
+
+// console.log (sayi1 + sayi2);
+
+// console.log (Number(sayi1) + Number(sayi2));
+// console.log (sayi1.toString + sayi2.toString);
+
+let isim = "mert murat";
+let soyad = "bakır";
+
+console.log(isim + " " +soyad);
+let sinavNotu = 30;
+
+let basarilimi = (sinavNotu >= 50);
+
+console.log(basarilimi);
+console.log(typeof basarilimi); // boolean
