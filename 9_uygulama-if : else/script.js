c7456ee28efb9f254433cbc0a9c783c38c5d0ea5
MertMuratBakir <125885626+MertMuratBakir@users.noreply.github.com>
2023-07-28 19:27:37 +0300

    if / else_uygulama

---
+++ 9_uygulama-if : else/script.js
@@ -0,0 +1,67 @@
+// 1-Bir sayının 10-50 arasında olup olmadığını kontol ediniz.
+
+// let sayi1 = 20;
+// if (sayi1 > 10 && sayi1 < 50) {
+//     console.log("sayı 10-50 arasındadır.")
+// } else {
+    // console.log("sayı 10-50 arasında değildir.");
+// }
+
+// 2-Bir sayının pozitif tek sayı olup olmadığını kontol ediniz.
+
+// let sayi = 21;
+// if (sayi % 2 == 1 && sayi > 0) 
+// {
+//     console.log("sayı pozitif tek sayıdır.");
+// }
+
+// "%" işareti ile sayının modunu alırız yani bölümünden kalan sayıya bakarız.
+
+// 3- x, y, z sayılarının büyüklük karşılaştırmasını yapınız. 
+
+// let x = 20, y = 50, z = 60;
+// if (x > y && x > z) {
+//     console.log("x en büyüktür.");
+// } else if (y > x && y > z) {
+//     console.log("y en büyüktür.");
+// } else if (z > x && z > y) {
+//     console.log("z en büyüktür.");
+// } else {
+//     console.log ("sayılar eşittir.");
+// }
+
+// 4- 2 vize ve 1 final notuna göre hesaplanan ortalama için;
+
+let vize1 = 10;
+let vize2 = 10;
+let final = 70;
+
+let ortalama = (((vize1 + vize2) / 2) * 0.4) + (final * 0.6);
+
+console.log("ortalamanız: " + ortalama)
+
+    // a-Eğer ortalama 50 ve üstündeyse geçti değilse kaldı yazsın.
+
+// if (ortalama >= 50) {
+//     console.log("geçtiniz.")
+// } else {
+//     console.log("kaldınız.")
+// }
+
+    // b-Geçmek için ortalama 50 bile olsa final notu en az 50 olmalıdır.
+
+// if (ortalama >= 50 && final >= 50) {
+//     console.log("geçtiniz.")
+// } else {
+//     console.log("kaldınız.")
+// }
+
+    //c-Finalden 70 alındığında ortalama 50'nin aldında olsa bile dersten geçilsin.
+
+// if (ortalama >= 50 || final >= 70) 
+// {
+//     console.log("geçtiniz.")
+// }
+
+
+
